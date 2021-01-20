b313e549f3ffdf97f7bf5210f511103cee248eb1
mandycortis <mandy.cortis.c10325@mcast.edu.mt>
2021-01-20 14:47:16 +0100

    KU4- PatrolBehaviousScript handles the AI and obstacles spawning, whilst calling the customObstacleMove script which handles the obstacles moving from 1 waypoint to another. Waypoints are instantiated randomly on runtime.

---
+++ Scripts/KU4PatrolBehaviourScript.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KU4PatrolBehaviourScript : MonoBehaviour
+{
+    public List<Transform> waypoints;
+
+    public Transform objectToMove;
+
+    [SerializeField]
+    GameObject obstaclesPrefab;
+
+    [SerializeField]
+    Transform obsParent;
+
+    [SerializeField]
+    GameObject ai;
+
+    [SerializeField]
+    List<Vector3> existingObstacles;
+
+    private float timer = 0.0f;
+    readonly private float waitTime = 0.1f;
+
+    private Vector3 RandomLocation()
+    {
+        Vector3 pos;
+        int x, y;
+
+        do
+        {
+            x = Random.Range(-49, 49);
+            y = Random.Range(-49, 49);
+
+            pos = new Vector3(x + 1.5f, y + 1.5f);
+        }
+        while (existingObstacles.Contains(new Vector3(x, y)));
+
+        return pos;
+    }
+
+    public void SpawnAI()
+    {
+        Vector3 pos = RandomLocation();
+        ai.SetActive(true);
+        Instantiate(ai, pos, Quaternion.identity);
+        objectToMove = GameObject.FindGameObjectWithTag("AI").transform;
+    }
+
+
+    public void AddObstacle()
+    {
+        for (int counter = 0; counter < 3; counter++)
+        {
+            Vector3 pos = RandomLocation();
+            existingObstacles.Add(pos);
+            GameObject obs = Instantiate(obstaclesPrefab, pos, Quaternion.identity);
+            obs.transform.parent = obsParent;
+
+            GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+            foreach(GameObject obstacle in obstacles)
+            {
+                obstacle.GetComponent<customObstacleMove>().enabled = true;
+            }
+        }
+    }
+
+
+
+    IEnumerator moveMe()
+    {
+        //for(int counter=0;counter<waypoints.size;counter++)
+        foreach(Transform mytransform in waypoints)
+        {
+            while(Vector3.Distance(objectToMove.position,mytransform.position)>0.1f)
+            {
+                //1 unit towards the first one
+                objectToMove.position = Vector3.MoveTowards(objectToMove.position,
+                mytransform.position,1f);
+
+                yield return new WaitForSeconds(0.1f);        
+            }
+            
+            yield return null;
+        }
+        
+        yield return null;
+    }
+
+
+
+    void Start()
+    {
+        SpawnAI();
+        StartCoroutine(moveMe());
+        AddObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            GameObject.Find("AStarGrid").GetComponent<AstarPath>().Scan();
+            timer = waitTime;
+        }
+    }
+}
---
+++ Scripts/customObstacleMove.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customObstacleMove : MonoBehaviour
+{
+    public List<Transform> obsWaypoints;
+    public Transform objectToMove;
+
+    [SerializeField]
+    GameObject obstacles;
+
+    [SerializeField]
+    Transform obsParent;
+
+    [SerializeField]
+    GameObject wpParent;
+
+    [SerializeField]
+    GameObject obsWP;
+
+    [SerializeField]
+    List<Vector3> existingObstacles;
+
+
+    public int wayPointCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wpParent = new GameObject("ObsWaypoints");
+        wpParent.transform.position = new Vector3(0, 0);
+        SpawnWP();
+        
+    }
+
+    private Vector3 RandomLocation()
+    {
+        Vector3 pos;
+        int x, y;
+
+        do
+        {
+            x = Random.Range(-49, 49);
+            y = Random.Range(-49, 49);
+
+            pos = new Vector3(x + 1.5f, y + 1.5f);
+        }
+        while (existingObstacles.Contains(new Vector3(x, y)));
+
+        return pos;
+    }
+
+    public void SpawnWP()
+    {
+        for (int counter = 0; counter < 2; counter++)
+        {
+            Vector3 pos = RandomLocation();
+            existingObstacles.Add(pos);
+            GameObject wp = Instantiate(obsWP, pos, Quaternion.identity);
+            
+            obsWaypoints.Add(wp.transform);
+            wp.transform.parent = wpParent.transform;
+
+        }
+        StartCoroutine(moveMe());
+    }
+
+    IEnumerator moveMe()
+    {
+        //for(int counter=0;counter<waypoints.size;counter++)
+        foreach (Transform mytransform in obsWaypoints)
+        {
+            while (Vector3.Distance(objectToMove.position, mytransform.position) > 0.1f)
+            {
+                //1 unit towards the first one
+                objectToMove.position = Vector3.MoveTowards(objectToMove.position,
+                mytransform.position, 1f);
+
+                yield return new WaitForSeconds(0.1f);
+            }
+            wayPointCounter++;
+            if(wayPointCounter >= obsWaypoints.Count)
+            {
+                StopCoroutine(moveMe());
+                wayPointCounter = 0;
+                StartCoroutine(moveMe());
+            }
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
