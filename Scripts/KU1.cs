0ed7e2714440aee706ac5b4d93b0c6d2d531ef7c
mandycortis <mandy.cortis.c10325@mcast.edu.mt>
2021-01-20 10:49:16 +0100

    KU1-10 AI bots are randomly placed in the grid by updating the SpawnAI() method to contian a for loop which instantiates an AI until counter reaches 10. StartAI() method enables the customAIMoveScript which makes the AI move towards the Target. The Start() method then calls the SpawnAI() and StartAI() methods.

---
+++ Scripts/KU1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KU1 : MonoBehaviour
+{
+    [SerializeField]
+    List<Vector3> existingAIs;
+
+    [SerializeField]
+    GameObject ai;
+
+    void Start()
+    {
+        existingAIs = new List<Vector3>();
+        GameObject.Find("AStarGrid").GetComponent<AstarPath>().Scan();
+        SpawnAI();
+        StartAI();
+    }
+
+    private Vector3 RandomLocation()
+    {
+        Vector3 pos;
+        int x, y;
+
+        do
+        {
+            x = Random.Range(-49, 49);
+            y = Random.Range(-49, 49);
+
+            pos = new Vector3(x + 1.5f, y + 1.5f);
+        }
+        while (existingAIs.Contains(new Vector3(x, y)));
+
+        return pos;
+    }
+
+    public void SpawnAI()
+    {
+        ai.SetActive(true);
+        for (int counter = 0; counter < 9; counter++)
+        {
+            Vector3 pos = RandomLocation();
+            existingAIs.Add(pos);
+            GameObject ais = Instantiate(ai, pos, Quaternion.identity);
+        }
+    }
+
+
+    public void StartAI()
+    {
+
+        GameObject[] aim = GameObject.FindGameObjectsWithTag("AI");
+        for (int i = 0; i < aim.Length; i++)
+        {
+            aim[i].GetComponent<customAIMoveScript>().enabled = true;
+        }
+    }
+}
+
+/* KU1: A* pathfinding pro implements this functionality using a specific feature => LOCAL AVOIDANCE 
+        : it is based on RVO: Reciprocal Velocity Obstacles which is divided into 2 parts
+            :Core simulation code and Unity interface
+ */
